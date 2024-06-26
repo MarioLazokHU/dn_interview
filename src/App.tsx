97d62bf6e3ced60fa18b91e87b29561ff3c62096
Cynthia <lazokmarios7@gmail.com>
2024-06-26 18:12:29 +0200

    feat: add zod validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import BlogPost from "./BlogPost";
+import { Typography } from "@mui/material";
 import CreatePost from "./CreatePost";
 
 function App() {
   return (
     <>
-    <div className="main-container">
-      <CreatePost />
-      <BlogPost />
+      <div className="flex h-screen flex-col justify-center items-center gap-20">
+        <Typography variant="h2">Mario's blog site</Typography>
+        <div className="w-fit">
+          <CreatePost />
+        </div>
       </div>
     </>
   );
